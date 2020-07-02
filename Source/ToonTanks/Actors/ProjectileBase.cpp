88face4b274c888c8974c4e4fdb7d0fa8f800451
j01 <jevalen4868@yahoo.com>
2020-07-02 12:57:42 -0500

    148: Creating GameMode classes

--- Source/ToonTanks/Actors/ProjectileBase.cpp
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -4,6 +4,7 @@
 #include "ProjectileBase.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
@@ -11,6 +12,7 @@ AProjectileBase::AProjectileBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
 	RootComponent = ProjectileMesh;
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
 
@@ -24,4 +26,18 @@ AProjectileBase::AProjectileBase()
 void AProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void AProjectileBase::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, FVector NormalImpulse, const FHitResult &Hit) {
+	// try to get a reference to the owning class
+	AActor *MyOwner{ GetOwner() };
+	if (!MyOwner) {
+		return;
+	}
+	// If the other actor isn't self or owner && exists, then apply damage.
+	if (OtherActor != NULL && OtherActor != this && OtherActor != MyOwner) {
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwner->GetInstigatorController(), this, DamageType);
+	}
+	// Do a bunch of effects here during polish phase.
+	Destroy();
 }
